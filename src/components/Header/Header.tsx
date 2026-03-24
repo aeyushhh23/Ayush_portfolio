06b605e9c21a9f0142b9eea79eabfa3772a73b13
aeyushhh23 <142649066+aeyushhh23@users.noreply.github.com>
2026-03-25 01:14:50 +0530

    Update header name from 'Priyanshu' to 'Ayush Kumar'

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,7 +16,7 @@ export function Header() {
     <Container className="header-fixed">
       <Router>
         <HashLink smooth to="#home" className="logo">
-          <span>{"<Priyanshu "}</span>
+          <span>{"<Ayush Kumar "}</span>
           <span>{" Pandey/>"}</span>
         </HashLink>
         <input
